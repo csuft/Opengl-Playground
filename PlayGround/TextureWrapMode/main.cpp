0aae424f62050e327de1c21c5a014f86abe1953e
zzkhnb <zhangzhongke@arashivision.com>
2017-06-03 12:07:07 +0800

    use texture for sphere

--- PlayGround/TextureWrapMode/main.cpp
+++ PlayGround/TextureWrapMode/main.cpp
@@ -7,6 +7,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/constants.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <SOIL.h>
 using namespace glm;
 
 #include "shader.hpp"
@@ -60,6 +61,14 @@ int main(void)
 	// Accept fragment if it closer to the camera than the former one
 	glDepthFunc(GL_LESS);
 
+	GLuint textureID = SOIL_load_OGL_texture("texture.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y);
+	if (textureID  == 0)
+	{
+		std::cout << "Failed to load OpenGL texture." << std::endl;
+		glfwTerminate();
+		return -4;
+	}
+
 	GLuint VertexArrayID;
 	glGenVertexArrays(1, &VertexArrayID);
 	glBindVertexArray(VertexArrayID);
@@ -80,9 +89,16 @@ int main(void)
 	glGenBuffers(1, &indices_buffer);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices_buffer);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, vert_indices.size()*sizeof(GLuint), vert_indices.data(), GL_STATIC_DRAW);
+
+	// UVs
+	GLuint uv_buffer;
+	glGenBuffers(1, &uv_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, uv_buffer);
+	glBufferData(GL_ARRAY_BUFFER, texcoords.size()*sizeof(GLfloat), texcoords.data(), GL_STATIC_DRAW);
 	 
 	GLuint programID = LoadShaders("simple.vert", "simple.frag"/*, "simple.geom", true*/);
 	GLuint mvpID = glGetUniformLocation(programID, "MVP");
+	GLuint samplerID = glGetUniformLocation(programID, "texSampler");
 	 
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 	glViewport(0, 0, 1024, 768);
@@ -103,10 +119,22 @@ int main(void)
 		MVP = projectionMat * viewMat * modelMat;
 		glUniformMatrix4fv(mvpID, 1, GL_FALSE, glm::value_ptr(MVP));
 
+		// bind texture unit 0
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glUniform1i(samplerID, 0);
+
+		// upload vertices
 		glEnableVertexAttribArray(1);
 		glBindBuffer(GL_ARRAY_BUFFER, vert_buffer);
 		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);  
 
+		// upload uv coordinates
+		glEnableVertexAttribArray(2);
+		glBindBuffer(GL_ARRAY_BUFFER, uv_buffer);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+		// use indices
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices_buffer);
 		glDrawElements(GL_LINE_STRIP, vert_indices.size(), GL_UNSIGNED_INT, (void*)0); // we use index buffer, so set it to null.  
 		//glDrawArrays(GL_LINE_STRIP, 0, vert_coords.size());
